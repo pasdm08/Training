c192df45ae1c276b5e8dcd86ef8b00836798494e
pasdm08 <pdm5843@gmail.com>
2019-10-08 10:35:01 +0200

    operazioni i/o(aggiunta interazione)

--- esercizio.cpp
+++ esercizio.cpp
@@ -6,6 +6,13 @@ int main() {
 	
 	int a, b, c;
 	
+	cout<<"inserisci numero"<<endl;
+	cin>>a;
+	cout<<"inserisci numero"<<endl;
+	cin>>b;
+	cout<<"la somma dei due numeri e': "<<c<<endl;
+	
+	
 	
 	
 }
